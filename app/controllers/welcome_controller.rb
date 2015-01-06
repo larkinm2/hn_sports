f398cecf2c2385dbbbad20108636b379642467eb
larkinm2 <larkinm2@gmail.com>
2015-01-05 22:36:37 -0500

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,36 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def sign
+  end
+
+  def tee
+  end
+
+  def park
+  end
+
+  def clinic
+  end
+
+  def basketball
+  end
+
+  def football
+  end
+
+  def swimming
+  end
+
+  def soccer
+  end
+
+
+end
